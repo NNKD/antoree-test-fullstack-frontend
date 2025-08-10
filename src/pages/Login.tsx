276c66b477bbee53c85f95ade745d190e1e98d30
NNKD <duynguyenngockhanh@gmail.com>
2025-08-10 10:48:02 +0700

    feat(login): responsive login UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,26 +4,26 @@ import {Link} from "react-router-dom";
 export default function Login() {
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className="w-full max-w-md p-10 mx-auto rounded border-2 border-gray-300 shadow-[0_0_3px_2px_#ddd]">
-                <p className="text-center text-2xl text-green-400 font-bold">Sign in</p>
-                <p className="text-center text-base text-gray-500 my-4">Welcome back! Ready to learn?</p>
-                <div className="group flex items-center my-8 border-2 border-gray-300 rounded focus-within:border-green-400">
-                    <FaEnvelope className="w-1/5 text-gray-300 group-focus-within:text-green-400" />
-                    <input type="email" name="email" placeholder="Email" className="rounded bg-transparent outline-none p-2 w-full"/>
+            <div className="md:w-3/4 lg:w-1/3 p-10 mx-auto rounded border-2 border-gray-300 shadow-[0_0_3px_2px_#ddd]">
+                <p className="text-center text-2xl md:text-4xl lg:text-2xl 2xl:text-4xl text-green-400 font-bold">Sign in</p>
+                <p className="text-center text-base md:text-2xl lg:text-base 2xl:text-2xl text-gray-500 my-4 2xl:my-6">Welcome back! Ready to learn?</p>
+                <div className="group flex items-center my-8 md:my-10 lg:my-8 2xl:my-10 border-2 border-gray-300 rounded focus-within:border-green-400">
+                    <FaEnvelope className="w-1/5 md:text-2xl lg:text-base 2xl:text-2xl text-gray-300 group-focus-within:text-green-400" />
+                    <input type="email" name="email" placeholder="Email" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"/>
                 </div>
-                <div className="group flex items-center my-8 border-2 border-gray-300 rounded focus-within:border-green-400">
-                    <FaLock className="w-1/5 text-gray-300 group-focus-within:text-green-400"/>
-                    <input type="password" name="password" placeholder="Password" className="rounded bg-transparent outline-none p-2 w-full"/>
+                <div className="group flex items-center my-8 2xl:my-10 border-2 border-gray-300 rounded focus-within:border-green-400">
+                    <FaLock className="w-1/5 md:text-2xl lg:text-base 2xl:text-2xl text-gray-300 group-focus-within:text-green-400"/>
+                    <input type="password" name="password" placeholder="Password" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"/>
                 </div>
 
-                <div className="mx-auto my-4 w-fit bg-green-400 py-2 px-4 rounded font-bold text-white text-base
+                <div className="mx-auto my-4 md:my-6 lg:my-4 2xl:my-6 w-fit bg-green-400 py-2 md:py-4 lg:py-2 2xl:py-4 px-4 md:px-8 lg:px-4 2xl:px-8 rounded font-bold text-white text-base md:text-2xl lg:text-base 2xl:text-2xl
                                 transition-all ease-in-out cursor-pointer hover:bg-green-500 hover:shadow-[0_0_3px_2px_#ccc]">
                     Sign in
                 </div>
 
                 <Link
                     to="/register"
-                    className="block ml-auto w-fit text-base text-blue-600 hover:underline hover:text-blue-800 transition-all ease-in-out">
+                    className="block ml-auto w-fit text-base md:text-2xl lg:text-base 2xl:text-2xl text-blue-600 hover:underline hover:text-blue-800 transition-all ease-in-out">
                     Don't have an account? Sign up now
                 </Link>
 
