30b8474245364dad9470507e9a63418e281cd554
NNKD <duynguyenngockhanh@gmail.com>
2025-08-12 14:35:54 +0700

    feat(register): add API connection, show notice

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,19 @@
 import {FaEnvelope, FaLock} from "react-icons/fa6";
 import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {register, sendMailVerificationCode} from "../utils/api.ts";
+import NoticeUI from "../components/NoticeUI.tsx";
+import type {UserRegisterDTO} from "../dtos/user-dto.ts";
 
 export default function Register() {
     const [timeLeft, setTimeLeft] = useState(0)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [code, setCode] = useState("")
+    const [message, setMessage] = useState("")
+    const [type, setType] = useState("")
+    const [loading, setLoading] = useState(false)
 
     // handle countdown time
     useEffect(() => {
@@ -17,10 +27,36 @@ export default function Register() {
 
     }, [timeLeft])
 
-    // handle send code
-    const handleSendCode = () => {
+    const handleSendCode = async () => {
         if (timeLeft == 0) {
             setTimeLeft(60)
+            if (email == "") {
+                setType("warning")
+                setMessage("Email cannot be empty")
+                return;
+            }
+            const result = await sendMailVerificationCode(email)
+            setType(result?.status || "info")
+            setMessage(result?.message)
+        }
+    }
+
+    const handleRegister = async () => {
+        setLoading(true)
+        if (password !== confirmPassword) {
+            setType("error")
+            setMessage("Passwords don't match")
+            setLoading(false)
+        }else {
+            const userRegisterDTO: UserRegisterDTO = {
+                email,
+                password,
+                code,
+            }
+            const result = await register(userRegisterDTO);
+            setType(result?.status || "info")
+            setMessage(result?.message)
+            setLoading(false)
         }
     }
 
@@ -33,10 +69,12 @@ export default function Register() {
                 <p className="text-center text-base md:text-2xl lg:text-base 2xl:text-2xl text-gray-500 mt-2 mb-4">Create an account and start learning</p>
                 <div className="group flex items-center border-2 border-gray-300 rounded focus-within:border-green-400">
                     <FaEnvelope className="w-1/5 md:text-2xl lg:text-base 2xl:text-2xl text-gray-300 group-focus-within:text-green-400" />
-                    <input type="email" name="email" placeholder="Email" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"/>
+                    <input type="email" name="email" placeholder="Email" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"
+                    value={email} onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="group flex justify-between my-6 md:my-8 lg:my-6 2xl:my-8 border-2 border-gray-300 rounded focus-within:border-green-400">
-                    <input type="text" name="code" placeholder="Code" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 md:text-2xl lg:text-base 2xl:text-2xl"/>
+                    <input type="text" name="code" placeholder="Code" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 md:text-2xl lg:text-base 2xl:text-2xl"
+                    value={code} onChange={e => setCode(e.target.value)} />
                     <div className={`min-w-[120px] flex items-center justify-center font-bold px-4
                                     ${timeLeft == 0 ? "bg-green-400 text-white transition-all ease-in-out cursor-pointer hover:bg-green-500 hover:shadow-[0_0_3px_2px_#ccc]" : "pointer-events-none bg-gray-300 text-gray-400"} }`}
                         onClick={handleSendCode}>
@@ -46,17 +84,20 @@ export default function Register() {
 
                 <div className="group flex items-center my-6 md:my-8 lg:my-6 2xl:my-8 border-2 border-gray-300 rounded focus-within:border-green-400">
                     <FaLock className="w-1/5 md:text-2xl lg:text-base 2xl:text-2xl text-gray-300 group-focus-within:text-green-400"/>
-                    <input type="password" name="password" placeholder="Password" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"/>
+                    <input type="password" name="password" placeholder="Password" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"
+                    value={password} onChange={e => setPassword(e.target.value)} />
                 </div>
 
                 <div className="group flex items-center border-2 border-gray-300 rounded focus-within:border-green-400">
                     <FaLock className="w-1/5 md:text-2xl lg:text-base 2xl:text-2xl text-gray-300 group-focus-within:text-green-400"/>
-                    <input type="password" name="confirm-password" placeholder="Confirm Password" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"/>
+                    <input type="password" name="confirm-password" placeholder="Confirm Password" className="rounded bg-transparent outline-none p-2 md:p-4 lg:p-2 2xl:p-4 w-full md:text-2xl lg:text-base 2xl:text-2xl"
+                    value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </div>
 
-                <div className="mx-auto my-4 md:my-6 lg:my-4 2xl:my-6 w-fit bg-green-400 py-2 md:py-4 lg:py-2 2xl:py-4 px-4 md:px-8 lg:px-4 2xl:px-8 rounded font-bold text-white text-base md:text-2xl lg:text-base 2xl:text-2xl
-                                transition-all ease-in-out cursor-pointer hover:bg-green-500 hover:shadow-[0_0_3px_2px_#ccc]">
-                    Sign up
+                <div className={`mx-auto my-4 md:my-6 lg:my-4 2xl:my-6 w-fit py-2 md:py-4 lg:py-2 2xl:py-4 px-4 md:px-8 lg:px-4 2xl:px-8 rounded font-bold text-base md:text-2xl lg:text-base 2xl:text-2xl                                
+                                ${loading ? "pointer-events-none bg-gray-300 text-gray-400" : "bg-green-400 text-white transition-all ease-in-out cursor-pointer hover:bg-green-500 hover:shadow-[0_0_3px_2px_#ccc]"}`}
+                    onClick={handleRegister}>
+                    {loading ? 'Loading' : 'Sign up'}
                 </div>
 
                 <Link
@@ -67,6 +108,9 @@ export default function Register() {
 
 
             </div>
+
+            <NoticeUI mess={message} type={type} />
+
         </div>
     )
 }
---
+++ src/utils/api.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import {env} from "../config/env.ts";
+import type {UserRegisterDTO} from "../dtos/user-dto.ts";
+
+function getErrorMessage(error: unknown): string {
+    if (axios.isAxiosError(error)) {
+        return error.response?.data?.message ?? 'No message from server';
+    }
+    return 'Unknown error';
+}
+
+export const sendMailVerificationCode = async (email: string) => {
+    try {
+        const response = await axios.post(`${env.API_URL}/mails/send-code`, {
+            email,
+        },)
+
+        console.log(response);
+        if (response.data.statusCode == 200 && response.data.status == 'Success') {
+            return {status: 'success', message: response.data.message};
+        }
+
+    }catch (error: unknown) {
+        console.log(error)
+        return {status: 'success', message: getErrorMessage(error)};
+    }
+}
+
+export const register = async (userRegisterDTO: UserRegisterDTO) => {
+    try {
+        const response = await axios.post(`${env.API_URL}/users/register`, userRegisterDTO)
+
+        if (response.data.statusCode == 200 && response.data.status == 'Success') {
+            return {status: 'success', message: response.data.message};
+        }
+
+    }catch (error: unknown) {
+        console.log(error)
+        return {status: 'success', message: getErrorMessage(error)};
+    }
+}
